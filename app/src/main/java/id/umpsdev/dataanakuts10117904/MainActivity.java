1d7e7affa6b0b9f22e7f8a49ef6a83f1a218fc4e
umpkintoy <umpkintoy@users.noreply.github.com>
2020-05-16 16:57:12 +0700

    Create MainActivity.java

---
+++ app/src/main/java/id/umpsdev/dataanakuts10117904/MainActivity.java
@@ -0,0 +1,21 @@
+package id.umpsdev.dataanakuts10117904;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+Nama    : Siti Safira Nadifa
+NIM     : 10117904
+Kelas   : IF-6K
+Tanggal Pengerjaan : 16-05-2020
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
